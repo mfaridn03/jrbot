cd325df63853d5837062b0eb2237c33958804d1c
mfaridn03 <35685904+mfaridn03@users.noreply.github.com>
2019-02-22 07:14:10 +0800

    removed moderation; added info

--- cogs/Info.py
+++ cogs/Info.py
@@ -3,123 +3,10 @@ import discord
 import datetime
 
 
-class PersonalServerOnly(commands.CheckFailure):
-    """
-    Moderation commands are only for personal server
-    """
-    pass
-
-
-def pguild():
-    async def predicate(ctx):
-        if ctx.guild.owner_id == 191036924570501120:
-            return True
-        raise PersonalServerOnly("This command is only for personal server")
-
-class Moderation:
+class Info:
     def __init__(self, bot):
         self.bot = bot
-        self.logging_channel = 537983096633688075
-    
-    @pguild()
-    @commands.has_permissions(kick_members=True)
-    @commands.command(mame='kick')
-    async def kick(self, ctx, member, *, reason=None):
-        """
-        Kicks a member
 
-        Example usages:
-        - f.kick @BadGuy#1010
-        - f.kick 191036924570501120
-        - f.kick Sicko#1337 being annoying
-        """
-        if not reason:
-            reason = 'Unspecified'
-
-        try:
-            target = await commands.MemberConverter().convert(ctx, member)
-        except commands.BadArgument:
-            return await ctx.send(f"Member {member} not found")
-        
-        await target.send(f'You have been kicked out from {ctx.guild.name} for reason: **{reason}**')
-        await target.kick(reason=reason)
-        await ctx.send(f"👍 {target} was kicked because of: **{reason}**")
-        
-        log_channel = discord.utils.get(ctx.guild.channels, id=self.logging_channel)
-        emb = discord.Embed(title='Member kick', timestamp=datetime.datetime.utcnow(), colour=discord.Colour.red())
-        emb.add_field(name='Member', value=f'{target.mention} ({target} - {target.id})', inline=False)
-        emb.add_field(name='Reason', value=reason, inline=False)
-        emb.set_footer(text=f'Mod: {ctx.author}', icon_url=ctx.author.avatar_url)
-        await ctx.message.add_reaction('✅')
-        await log_channel.send(embed=emb)
-    
-    @pguild()
-    @commands.has_permissions(ban_members=True)
-    @commands.command(name='ban')
-    async def ban(self, ctx, member, *, reason=None):
-        """
-        Bans member
-
-        Usage examples:
-        - f.ban @User#2932 attempted raid
-        - f.ban 463962914274148363
-        """
-        if not reason:
-            reason = 'Unspecified'
-
-        try:
-            target = await commands.MemberConverter().convert(ctx, member)
-        except commands.BadArgument:
-            return await ctx.send(f'Member {member} not found')
-        
-        await target.send(f'You have been banned from {ctx.guild.name} for reason: **{reason}**')
-        await ctx.guild.ban(target, reason=reason)
-        await ctx.send(f"👍 {target} ({target.id}) was banned because of: **{reason}**")
-        
-        log_channel = discord.utils.get(ctx.guild.channels, id=self.logging_channel)
-        emb = discord.Embed(title='Ban', timestamp=datetime.datetime.utcnow(), colour=discord.Colour.red())
-        emb.add_field(name='Member', value=f'{target.mention} ({target} - {target.id})', inline=False)
-        emb.add_field(name='Reason', value=reason, inline=False)
-        emb.set_footer(text=f'Mod: {ctx.author}', icon_url=ctx.author.avatar_url)
-        await ctx.message.add_reaction('✅')
-        await log_channel.send(embed=emb)
-    
-    @pguild()
-    @commands.has_permissions(ban_members=True)
-    @commands.command(name='unban')
-    async def unban(self, ctx, user_id, *, reason=None):
-        """
-        Unbans a member by their id
-
-        Usage examples:
-        - f.unban 326326213624987649 time limit
-        - f.unban 298166754331459586
-        """
-        if not reason:
-            reason = 'Unspecified'
-        
-        user = discord.Object(id=user_id)
-        try:
-            await ctx.guild.unban(user, reason=reason)
-        except discord.NotFound:
-            return await ctx.send(f'User {user_id} either cannot be found or is not banned')
-        
-        log_channel = discord.utils.get(ctx.guild.channels, id=self.logging_channel)
-        emb = discord.Embed(title='Unban', timestamp=datetime.datetime.utcnow(), colour=discord.Colour.orange())
-        emb.add_field(name='User', value=f'{user.name}#{user.discriminator} ({user.id})', inline=False)
-        emb.add_field(name='Reason', value=reason, inline=False)
-        emb.set_footer(text=f'Mod: {ctx.author}', icon_url=ctx.author.avatar_url)
-        await ctx.message.add_reaction('✅')
-        await log_channel.send(embed=emb)
-    
-    @pguild()
-    @commands.is_owner()
-    @commands.command(name='echo', aliases=['say'], hidden=True)
-    async def echo(self, ctx, *, msg):
-        await ctx.message.delete()
-        await ctx.send(msg)
-    
-    @pguild()
     @commands.command(name='verify', hidden=True)
     async def verify(self, ctx):
         if ctx.channel.id != 534754067998834688:
@@ -128,13 +15,13 @@ class Moderation:
         role = discord.utils.get(ctx.guild.roles, id=534273110023602177)
         await ctx.author.add_roles(role)
         await ctx.message.delete()
-    
+
     @commands.command(name='info', aliases=['userinfo'])
     async def userinfo(self, ctx, *, member=None):
         """
         Retrieves info about yourself, or a user
         <member> is optional
-        
+
         Usage examples:
         - f.info
         - f.info Sai#8985
@@ -147,7 +34,7 @@ class Moderation:
                 return await ctx.send(f'Member {member} not found')
         else:
             m = ctx.author
-        
+
         username = str(m)
         display_name = m.display_name
         col = m.colour
@@ -171,12 +58,12 @@ class Moderation:
         emb.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
 
         await ctx.send(embed=emb)
-    
+
     @commands.command(name='serverinfo')
     async def serverinfo(self, ctx):
         """
         Retrieves server info
-        
+
         Usage:
         - f.serverinfo
         """
@@ -198,7 +85,7 @@ class Moderation:
         vc_region = guild.region
         content_filter = guild.explicit_content_filter
         internals = f"Verification level: {verif}\nVoice region: {vc_region}\nContent filter: {content_filter}"
-        
+
         emb = discord.Embed(title='Server info',
                             description=f'**Name:** {name}\n**ID**: {guild.id}',
                             colour=discord.Colour.dark_teal(),
@@ -213,14 +100,14 @@ class Moderation:
         emb.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
         if 'icon' in icon:
             emb.set_thumbnail(url=icon)
-        
+
         await ctx.send(embed=emb)
-    
+
     @commands.command(name='avatar')
     async def avatar(self, ctx, member=None):
         """
         Get the avatar of yourself or a member
-        
+
         Usage examples:
         - f.avatar
         - f.avatar John12
@@ -234,7 +121,7 @@ class Moderation:
                 return await ctx.send(f'Member {member} not found')
         else:
             m = ctx.author
-            
+
         desc = f'[Link]({m.avatar_url})'
         emb = discord.Embed(title=f"{str(m)}'s avatar",
                             description=desc,
@@ -244,4 +131,4 @@ class Moderation:
 
 
 def setup(bot):
-    bot.add_cog(Moderation(bot))
+    bot.add_cog(Info(bot))
