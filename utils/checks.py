e8e416c7c07a253cdb4e20c59aeb0359f1d63183
mfaridn03 <35685904+mfaridn03@users.noreply.github.com>
2019-02-27 19:51:09 +0800

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -1,1 +1,12 @@
-sup
+import sqlite3
+
+conn = sqlite3.connect('Data.db')
+c = conn.cursor()
+
+class Checks:  # WIP
+    def __init__(self, bot):
+        self.bot = bot
+    
+    def has_char(self, user):
+        pass
+        
