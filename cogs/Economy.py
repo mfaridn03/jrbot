3314a6d4444f88607c73b8bf7c7dc1cf339751a9
mfaridn03 <35685904+mfaridn03@users.noreply.github.com>
2019-02-27 20:01:40 +0800

    Update Economy.py

--- cogs/Economy.py
+++ cogs/Economy.py
@@ -1,2 +1,25 @@
+import discord
+from discord.ext import commands
+import sqlite3
+from utils import checks
+
+conn = sqlite3.connect('Data.db')
+c = conn.cursor()
+
 class Economy:
-    pass
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command(name='create')
+    async def create(self, ctx):
+        """
+        Create your profile
+        
+        Usage:
+        - f.create
+        """
+        pass
+
+def setup(bot):
+    bot.add_cog(Economy(bot))
+    
