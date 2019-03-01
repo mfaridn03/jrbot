16bc879f0803b73428f7e147c96010fd678dfb5f
mfaridn03 <35685904+mfaridn03@users.noreply.github.com>
2019-03-01 23:14:31 +0800

    Update schema.sql

--- setup/schema.sql
+++ setup/schema.sql
@@ -1,1 +1,9 @@
+CREATE TABLE IF NOT EXISTS user_info (
+  userid BIGINT PRIMARY KEY,
+  creation_date NUMERIC DEFAULT (strftime('%s', 'now')
+);
 
+CREATE TABLE IF NOT EXISTS user_balance (
+  userid BIGINT NOT NULL REFERENCES user_info(userid) ON DELETE CASCADE,
+  balance NUMERIC NOT NULL DEFAULT 0
+);
