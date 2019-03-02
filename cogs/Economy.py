7fe0333c36efc36a759b41b35df5519bab0daaaa
mfaridn03 <35685904+mfaridn03@users.noreply.github.com>
2019-03-02 17:27:53 +0800

    Update Economy.py

--- cogs/Economy.py
+++ cogs/Economy.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+moai = "<:moai:532243816946597947>"
+
 
 class Economy(commands.Cog):
     def __init__(self, bot):
@@ -14,7 +16,39 @@ class Economy(commands.Cog):
         Usage:
         - f.create
         """
-        pass
+        async with ctx.typing():
+            q = f"INSERT INTO user_info (userid) VALUES {ctx.author.id};" \
+            f"INSERT INTO user_balance (balance) VALUES 100;" \
+            f"INSERT INTO user_profile (name, multiplier) VALUES ({ctx.author.name}, 1.0);"
+            self.bot.db.execute(q, many=True)
+        await ctx.send('Profile created!')
+    
+    @commands.command(name='bal', aliases=['balance'])
+    async def balance(self, ctx, member=None):
+        """
+        Retrieves your (or someone's) balance (server-wide search, not global)
+        <member> can be their name#discriminator, id, nickname or username
+        
+        Usage examples:
+        - f.bal
+        - f.bal Techno#2329
+        - f.bal 376911204481892352
+        """
+        target = member or ctx.author.id
+        try:
+            target = await commands.MemberConverter().convert(
+                ctx, target
+            )
+        except commands.NotFound:
+            return await ctx.send(
+                'Cannot find user'
+            )
+        res = self.bot.db.fetch(
+            'SELECT balance FROM user_balance WHERE userid=?', (ctx.author.id,)
+        )
+        await ctx.send(
+            f'`{target}`' has **{res}**{moai}
+        )
 
 def setup(bot):
     bot.add_cog(Economy(bot))
