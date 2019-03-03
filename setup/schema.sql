b358f71b28f4a1aa78b22e177781f5f337154ab7
mfaridn03 <35685904+mfaridn03@users.noreply.github.com>
2019-03-03 10:29:57 +0800

    Update schema.sql

--- setup/schema.sql
+++ setup/schema.sql
@@ -4,12 +4,12 @@ CREATE TABLE IF NOT EXISTS user_info (
 );
 
 CREATE TABLE IF NOT EXISTS user_balance (
-    userid BIGINT NOT NULL REFERENCES user_info(userid) ON DELETE CASCADE,
+    userid BIGINTREFERENCES user_info(userid) ON DELETE CASCADE,
     balance NUMERIC NOT NULL DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS user_profile (
-    userid BIGINT NOT NULL REFERENCES user_info(userid) ON DELETE CASCADE,
+    userid BIGINT REFERENCES user_info(userid) ON DELETE CASCADE,
     name TEXT UNIQUE NOT NULL,
     multiplier NUMERIC NOT NULL DEFAULT 0
 );
