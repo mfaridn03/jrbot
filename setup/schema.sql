34d11ec6c0d4801ee50f5c08f6843116ecb8ba91
mfaridn03 <35685904+mfaridn03@users.noreply.github.com>
2019-03-17 12:27:20 +0800

    gay away

--- setup/schema.sql
+++ setup/schema.sql
@@ -14,10 +14,6 @@ CREATE TABLE IF NOT EXISTS user_profile (
     creation_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS commands (
-    total_commands NUMERIC NOT NULL DEFAULT 0
-);
-
 CREATE TABLE IF NOT EXISTS guild_prefixes (
     guild_id NUMERIC NOT NULL,
     prefix VARCHAR(512) NOT NULL
