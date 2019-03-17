b0fbd728fa76afb3a1349927ee7cf3df06abdcd7
mfaridn03 <35685904+mfaridn03@users.noreply.github.com>
2019-03-17 08:57:03 +0800

    Update schema.sql

--- setup/schema.sql
+++ setup/schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS user_info (
     userid NUMERIC,
-    creation_date NUMERIC DEFAULT (strftime('%s', 'now'))
+    creation_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    commands_done NUMERIC NOT NULL DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS user_balance (
@@ -11,7 +12,16 @@ CREATE TABLE IF NOT EXISTS user_balance (
 CREATE TABLE IF NOT EXISTS user_profile (
     userid NUMERIC NOT NULL,
     name TEXT UNIQUE NOT NULL,
-    multiplier NUMERIC NOT NULL DEFAULT 0
+    multiplier NUMERIC NOT NULL DEFAULT 1,
+    inventory TEXT
 );
-  
-  
+
+CREATE TABLE IF NOT EXISTS commands (
+    total_commands NUMERIC NOT NULL DEFAULT 0,
+);
+
+CREATE TABLE IF NOT EXISTS guild_prefixes (
+    guild_id NUMERIC NOT NULL,
+    prefix VARCHAR(512) NOT NULL
+);
+    
