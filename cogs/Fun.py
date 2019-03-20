8366d96956b796180ea6a111cda78f63af04e3d9
mfaridn03 <35685904+mfaridn03@users.noreply.github.com>
2019-03-20 14:04:13 +0800

    migrate from json to ujson
    experimenting lmao

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,15 +1,15 @@
 import datetime
 import asyncio
-import json
 import os
 import urllib
 
-import requests
-import discord
-from discord.ext import commands
 from bs4 import BeautifulSoup
-from newsapi import NewsApiClient
+import discord
+import requests
+import ujson
 
+from discord.ext import commands
+from newsapi import NewsApiClient
 
 class Fun(commands.Cog):
     def __init__(self, bot):
@@ -31,7 +31,7 @@ class Fun(commands.Cog):
                 session = requests.session()
                 link = f'http://api.urbandictionary.com/v0/define?term={urllib.parse.quote(word)}'
                 req_link = session.get(link).text
-                req_json = json.loads(req_link)
+                req_json = ujson.loads(req_link)
 
                 if not req_json['list']:
                     return await ctx.send(f'No search results found for "**{word}**"')
@@ -76,7 +76,7 @@ class Fun(commands.Cog):
         try:
             async with ctx.typing():
                 req_link = session.get(link).text
-                req_json = json.loads(req_link)
+                req_json = ujson.loads(req_link)
 
                 city_name = req_json['location']['name']
                 region = req_json['location']['region']
@@ -132,7 +132,7 @@ class Fun(commands.Cog):
         try:
             async with ctx.typing():
                 req_link = session.get(link).text
-                req_json = json.loads(req_link)
+                req_json = ujson.loads(req_link)
 
                 limit = len(req_json['result']) - 1
                 title = req_json['result'][index]['title']
@@ -228,7 +228,7 @@ class Fun(commands.Cog):
                 od_app_key = self.bot.config['OD_APP_KEY']
                 header = {"app_id": od_app_id, 'app_key': od_app_key}
                 req_link = session.get(link, headers=header).text
-                req_json = json.loads(req_link)
+                req_json = ujson.loads(req_link)
                 etymology, examples = '', ''
                 et, ex = True, True
 
@@ -282,8 +282,8 @@ class Fun(commands.Cog):
                         icon_url=ctx.author.avatar_url
                     )
             await ctx.send(embed=emb)
-        except json.JSONDecodeError:
-            return await ctx.send("No results found")
+        except:
+            return await ctx.send('Error fetching results; Try checking command format')
     
     @commands.cooldown(1, 3, commands.BucketType.user)
     @commands.command(name='yt', aliases=['youtube'])
@@ -690,7 +690,7 @@ class Fun(commands.Cog):
 
         async with ctx.typing():
             req_link = session.get(link).text
-            req_json = json.loads(req_link)
+            req_json = ujson.loads(req_link)
 
             if req_json is None:
                 return await ctx.send('No rhyme/s found')
